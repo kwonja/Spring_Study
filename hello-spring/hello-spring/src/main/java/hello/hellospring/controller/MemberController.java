f88e31159c29d3ee4d0247a332ee4e6c759f7927
kwonja <kownja87@gmail.com>
2023-01-14 10:41:45 +0900

    study

--- hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,10 +1,15 @@
 package hello.hellospring.controller;
 
 
+import hello.hellospring.repository.domain.Member;
 import hello.hellospring.service.MemberSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -19,9 +24,26 @@ public class MemberController {
 
         this.memberSerivce=memberSerivce;
     }
-    @GetMapping("/members/new")
+    @GetMapping("/members/new")  //url을 치는것은 보통 getmapping 이라고 한다.
     public String createForm(){
-        return "mebmers/createMemberForm";
+        return "/createMemberForm";
     }
+    @PostMapping("/members/new") //MemberForm 에 어떻게 들어오는것 일까?
+    public String create(MemberForm form) //스프링이 자동으로 MemberForm 클래스 안에 setName함수를 진행시켜 name 값을 넣어준다
+                                          //그래서 formData의 name이랑 클래스의 변수랑 type이 같아야하는것!
+    {
+        Member member = new Member();
+        member.setName(form.getName()); //들어간 변수 멤버에 저장
 
+        memberSerivce.join(member);
+
+        return "redirect:/";  //홈화면으로 보내는것?
+    }
+    @GetMapping("/members")
+    public String list(Model model)
+    {
+        List<Member> members = memberSerivce.findMember();
+        model.addAttribute("members",members);
+        return "members/memberList";
+    }
 }
